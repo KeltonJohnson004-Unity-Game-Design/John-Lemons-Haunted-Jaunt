66e095af5d58cbdcac32df560ae563b24cfb91f1
keltonjohnson004 <52982698+keltonjohnson004@users.noreply.github.com>
2020-09-06 03:36:19 -0600

    Added Enemies

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 2f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+
+    bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
+    float m_Timer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    private void Update()
+    {
+        if(m_IsPlayerAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+        else if(m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
+    {
+        m_Timer += Time.deltaTime;
+
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+        if(m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Observer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Player;
+    public GameEnding gameEnding;
+
+    bool m_IsPlayerInRange;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform == Player)
+        {
+            m_IsPlayerInRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.transform == Player)
+        {
+            m_IsPlayerInRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(m_IsPlayerInRange)
+        {
+            Vector3 direction = Player.position - transform.position + Vector3.up;
+            Ray ray = new Ray(transform.position, direction);
+            RaycastHit raycastHit;
+
+            if(Physics.Raycast(ray, out raycastHit))
+            {
+                if(raycastHit.collider.transform == Player)
+                {
+                    gameEnding.CaughtPlayer();
+                }
+            }
+        }
+    }
+}
